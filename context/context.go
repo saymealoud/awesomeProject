e255818ac449d31befc5f3b84c568f36128744b1
zhaojiacheng <sayyoualoud@gmail.com>
2023-04-19 09:23:09 +0800

    fix:file name

--- context/context.go
+++ context/context.go
@@ -7,6 +7,15 @@ import (
 	"time"
 )
 
+/*
+*
+select+channel 的方案，Context 方案主要有 4 个改动点。
+
+watchDog 的 stopCh 参数换成了 ctx，类型为 context.Context。
+原来的 case <-stopCh 改为 case <-ctx.Done()，用于判断是否停止。
+使用 context.WithCancel(context.Background()) 函数生成一个可以取消的 Context，用于发送停止指令。这里的 context.Background() 用于生成一个空 Context，一般作为整个 Context 树的根节点。
+原来的 stopCh <- true 停止指令，改为 context.WithCancel 函数返回的取消函数 stop()。
+*/
 func main() {
 
 	var wg sync.WaitGroup
